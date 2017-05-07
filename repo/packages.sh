565cbf51deaf1f7f498a78c93f479afaaec4622a
GlossyPanther <charlieriesner@icloud.com>
2017-05-07 20:15:16 +0100

    Update
    Update home page and remove old files.

--- repo/packages.sh
+++
@@ -1,3 +0,0 @@
-dpkg-deb -Zgzip -b VeexExtend
-
-mv VeexExtend.deb VeexExtend.`date +"%Y%m%d%H%M"`.deb
--- repo/update.sh
+++
@@ -1,7 +0,0 @@
-find . -name '.DS_Store' -type f -delete
-
-./remove.sh
-./packages.sh
-
-dpkg-scanpackages -m . /dev/null >Packages
-bzip2 Packages
